58cf673967ba3738fb46239b5dc9952e7bb4818f
Jaime Landazuri <jaume.lan@gmail.com>
2022-02-13 17:02:11 -0300

    testing routes

--- backend/routes/scrabble.js
+++ backend/routes/scrabble.js
@@ -2,5 +2,13 @@ const express = require('express');
 const router = express.Router();
 const scrabbleRules = require("../components/scrabbleRules");
 
+//middlewares
+router.use(express.json());
+
 //organizar aqui as rotas
-/* router.get() */
+router.get('/initialize', (req,res) => {
+    console.log(req);
+    res.send("You are in get route")
+})
+
+module.exports = router;
