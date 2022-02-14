1ab98550d250d432857fedd3eaa48b913c096ec3
Jaime Landazuri <jaume.lan@gmail.com>
2022-02-13 22:47:27 -0300

    criation of rules for initialize the game and sending the letters

--- backend/routes/scrabble.js
+++ backend/routes/scrabble.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const scrabbleRules = require("../components/scrabbleRules");
+const scrabblePortugues = new scrabbleRules;
 
 //middlewares
 router.use(express.json());
+router.use(express.urlencoded({extended:true}));
 
 //organizar aqui as rotas
 router.get('/initialize', (req,res) => {
-    console.log(req);
-    res.send("You are in get route")
+        
+    res.send(scrabblePortugues.chooseLetters())
+})
+.get('/shuffleletters/:whichplayer', (req, res) => {
+    let player = req.params;
+    console.log(player);
+    res.send(scrabblePortugues.shuffleLetters(player));
+
 })
 
 module.exports = router;
