3711f231e6d4f9c1d4c551bbb4d6cba5121d2002
Jaime Landazuri <jaume.lan@gmail.com>
2022-02-15 10:50:09 -0300

    boardgame first creation

--- frontend/pages/game/index.js
+++ frontend/pages/game/index.js
@@ -6,6 +6,20 @@
 // }
 
 $(document).ready(function() {
+  function boardCreator() {
+    for (let i = 0; i < 15; i++) {
+      let newRow = $('<div class="row"></div>');
+      for (let j = 0; j < 15; j++) {
+          let newCell = $('<div class="cell"></div>');
+          newCell.attr("row-number", i);
+          newCell.attr("column-number", j);
+          newRow.append(newCell);
+      }
+      $('#gameboard').append(newRow);
+    }
+  }
+  boardCreator();
+  
   $("#request").on("click", function () {
     const word = $("#word").val();
     $.ajax({url:`https://api.dicionario-aberto.net/near/${word}`})
