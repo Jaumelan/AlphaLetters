c8c9a06344e422e31799140e8428e9da0e2982d4
Washington Gacho <washington.gacho@dcomp.ufs.br>
2022-02-15 22:56:26 -0300

    test

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const port = 3000;
 const app = express();
-
+const test = [];
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
