56ed0e1c927767892939cb747ff834235550ca71
Jaumelan <49458468+Jaumelan@users.noreply.github.com>
2022-02-20 22:39:20 -0300

    more validations

--- frontend/pages/game/index.js
+++ frontend/pages/game/index.js
@@ -19,6 +19,8 @@ $(document).ready(function () {
     { letters: [], values: [], whichPlayer: "player2" },
     { lettersleft: 0 },
   ];
+  let firstMove = false; //default false
+  const boardRecord = [{xy:"88", letter:"M"}];
  /*  const url = {
     drawletters: "https://alpha-letters-backend.herokuapp.com/scrabble/drawletters/",
   }; */
@@ -50,8 +52,7 @@ $(document).ready(function () {
               firstPlayerTurn.is
             );
 
-            //console.log("dropped at row " + event.target.getAttribute("row-number") + " column " + event.target.getAttribute("column-number"))
-            //check as dropped on the board cell
+            //function to check when finished the game
             //game_board[find_board_pos(droppableID)].tile = draggableID; <---------------
             //wordCheck(word) <--------------
             //If the player remove the letter:
@@ -96,95 +97,152 @@ $(document).ready(function () {
     }
   }
 
-  //function to record letters place on the boardgame
-  function wordDraftCreator(id, letter, positionx, positiony, player) {
-    if (player) {
-      submitedLetters[0].player1.push(letter);
-      submitedLetters[0].playerId.push(id);
-      submitedLetters[2].boardId.push({ positionx, positiony });
-    } else {
-      submitedLetters[1].player2.push(letter);
-      submitedLetters[1].playerId.push(id);
-      submitedLetters[2].boardId.push({ positionx, positiony });
-    }
-  }
-
-  //function to delete letters from draft when removed from the boardgame
-  function takeWordFromDraft(id, player) {
-    if (player) {
-      let index = submitedLetters[0].playerId.indexOf(id);
-      if (index > -1) {
-        submitedLetters[0].player1.splice(index, 1);
-        submitedLetters[0].playerId.splice(index, 1);
-        submitedLetters[2].boardId.splice(index, 1);
-      }
-    } else {
-      let index = submitedLetters[1].playerId.indexOf(id);
-      if (index > -1) {
-        submitedLetters[1].player2.splice(index, 1);
-        submitedLetters[1].playerId.splice(index, 1);
-        submitedLetters[2].boardId.splice(index, 1);
-      }
-    }
-  }
-
-  //function to rearrange array by column
-  function rearrangeRowbyColumn(player) {
-    if (player) {
-      let y = selectTheRow(submitedLetters[2].boardId);
-      let copyY = [...y];
-      let orderedY = copyY.sort((a, b) => a - b);
-      let rearrangedIndexes = [];
-      orderedY.forEach(item=> rearrangedIndexes.push(y.indexOf(item)) );
-      /* let orderedPos = rearrangedIndexes.map( i => submitedLetters[2].boardId[i]) */
-      submitedLetters[2].boardId = rearrangedIndexes.map( i => submitedLetters[2].boardId[i]);
-      /* let orderedLetters = rearrangedIndexes.map( i => submitedLetters[0].player1[i]); */
-      submitedLetters[0].player1 = rearrangedIndexes.map( i => submitedLetters[0].player1[i]);
-      submitedLetters[0].playerId = rearrangedIndexes.map( i => submitedLetters[0].playerId[i]);
-      return true
-    } else {
-      let y = selectTheRow(submitedLetters[2].boardId);
-      let copyY = [...y];
-      let orderedY = copyY.sort((a, b) => a - b);
-      let rearrangedIndexes = [];
-      orderedY.forEach(item=> rearrangedIndexes.push(y.indexOf(item)) )
-      /* let orderedPos = rearrangedIndexes.map( i => submitedLetters[2].boardId[i]) */
-      submitedLetters[2].boardId = rearrangedIndexes.map( i => submitedLetters[2].boardId[i]);
-      /* let orderedLetters = rearrangedIndexes.map( i => submitedLetters[0].player1[i]); */
-      submitedLetters[1].player2 = rearrangedIndexes.map( i => submitedLetters[1].player2[i]);
-      submitedLetters[1].playerId = rearrangedIndexes.map( i => submitedLetters[1].playerId[i]);
-      return true
-    }
+  //function to validate the moves
+  function validateTheMove(board_positions, move, direction) {
+    const allowedDirections = ["row" , "column"];
+    let valid = false;
+    let same_value;
+    let different_values;
+    let gapsExist = false;
+    let copy;
+    let diff;
     
+    if(!move) {
+      //if is the first move check if has one tile in the center of the board
+      board_positions.forEach(position=>{ 
+        if (position.positionx === "7" && position.positiony === "7") {
+          if ( allowedDirections.includes(direction) ) {
+            
+            console.log("entrei primeiro movimento avaliador");
+            valid = true;
+          }
+        }
+      })
+    } else if ( allowedDirections.includes(direction) ){
+      //if isn't the first move, then check if is in row or column direction
+      console.log("entrei no de linha-coluna")
+      //check if is next to a tile in the board
+      if( isNextToATile(board_positions) ) {
+        console.log("validei se está do lado")
+        valid = true;
+      }
+    }
+
+    if (!move) {
+      if (board_positions.length > 1) {
+        if (direction === "row") {
+          same_value = board_positions[0].positionx;
+          different_values = selectTheRow(board_positions);
+          copy = [...different_values];
+          copy.map(value => Number(value) );
+          for (let i = 0; i < copy.length ; i++) {
+            diff = copy[i+1] - copy[i];
+            if (diff > 1) {
+              gapsExist = true;
+              console.log("tem gaps")
+            }
+          }
+        } else {
+          same_value = board_positions[0].positiony;
+          different_values = selectTheColumn(board_positions);
+          copy = [...different_values];
+          copy.map(value => Number(value) );
+          for (let i = 0; i < copy.length ; i++) {
+            diff = copy[i+1] - copy[i];
+            if (diff > 1) {
+              gapsExist = true;
+              console.log("tem gaps")
+            } 
+          }
+        }
+      }
+    }
+
+    if (gapsExist) {
+      valid = false;
+    }
+    return valid;
   }
 
-  //function to rearrange array by row
-  function rearrangeColumnbyRow(player) {
-    if (player) {
-      let x = selectTheColumn(submitedLetters[2].boardId);
-      let copyX = [...x];
-      console.log(copyX)
-      let orderedX = copyX.sort( (a,b) => a - b );
-      let rearrangedIndexes = [];
-      orderedX.forEach( item => rearrangedIndexes.push(x.indexOf(item)) );
-      submitedLetters[2].boardId = rearrangedIndexes.map( i => submitedLetters[2].boardId[i] );
-      submitedLetters[0].player1 = rearrangedIndexes.map( i => submitedLetters[0].player1[i]);
-      submitedLetters[0].playerId = rearrangedIndexes.map( i => submitedLetters[0].playerId[i]);
-      return true
-    } else {
-      let x = selectTheColumn(submitedLetters[2].boardId);
-      let copyX = [...x];
-      let orderedX = copyX.sort( (a,b) => a - b );
-      let rearrangedIndexes = [];
-      orderedX.forEach( item => rearrangedIndexes.push(x.indexOf(item)) );
-      submitedLetters[2].boardId = rearrangedIndexes.map( i => submitedLetters[2].boardId[i] );
-      submitedLetters[1].player2 = rearrangedIndexes.map( i => submitedLetters[1].player2[i]);
-      submitedLetters[1].playerId = rearrangedIndexes.map( i => submitedLetters[1].playerId[i]);
-      return true
+  //function to check if the tiles are next to any placed tile on the board
+  function isNextToATile(tiles_positions) {
+    let answer = false;
+    let condition = "";
+    boardRecord.forEach( board_position => {
+      tiles_positions.forEach (tile_position => {
+        let xUp = (Number(tile_position.positionx) -1);
+        let xDown = (Number(tile_position.positionx) +1);
+        let yLeft = (Number(tile_position.positiony) -1);
+        let yRight = (Number(tile_position.positiony) +1);
+        let positionstoString = [ xUp, xDown , yLeft, yRight ];
+        let positions = [`${positionstoString[0]}${tile_position.positiony}`, `${positionstoString[1]}${tile_position.positiony}`, `${tile_position.positionx}${positionstoString[2]}`, `${tile_position.positionx}${positionstoString[3]}`];
+        if (board_position.xy === `${tile_position.positionx}${tile_position.positiony}`) {
+          condition = "over";
+          console.log(condition);
+        } else if ( positions.includes(board_position.xy) ) {
+          condition = "next";
+          console.log(condition);
+        }
+      })
+    })
+    if (condition === "next") {
+      answer = true;
     }
+
+    return answer
   }
 
   boardCreator();
+
+  //function to find gap between letters
+  function getGaps(placed_positions) {
+    let gaps = [];
+    let diff = 0;
+    let xy;
+    let gap_value;
+    let rowNumber;
+    let y_values;
+    let copy_y;
+
+    //check if is row or columns
+    if( findDirection(placed_positions) === "row" ) {
+      rowNumber = placed_positions[0].positionx;
+      //then get the column values
+      let y_values = selectTheRow(placed_positions);
+      copy_y = [...y_values];
+      copy_y.map(value => Number(value) ); 
+      
+      for (let i = 0; i < copy_y.length; i++) {
+        diff = copy_y[i+1] - copy_y[i];
+        if (diff>2) {
+          for(let j = 1; j < diff; j++) {
+            gap_value = copy_y[i] + j
+            xy = `${rowNumber}${gap_value}`
+            gaps.push(xy);
+          }
+        }
+      }
+    } else {
+      let columnNumber = placed_positions[0].positiony;
+      //get the row values
+      let x_values = selectTheColumn(placed_positions);
+      let copy_x = [...x_values];
+      copy_x.map(value => Number(value));
+      for (let i = 0; i < copy_x.length ; i++) {
+        diff = copy_x[i+1] - copy_x[i];
+        if(diff>2) {
+          for( let j = 1; j < diff; j++) {
+            gap_value = copy_x[i] + j;
+            xy = `${gap_value}${columnNumber}`
+            gaps.push(xy)
+          }
+        }
+      }
+    }
+
+    return gaps
+  }
+
   //programação do modal
   const avatars = Array.from(document.getElementsByClassName("avatars"));
   const avatarIDs = [
@@ -235,6 +293,7 @@ $(document).ready(function () {
     { id: "deck1_piece5", letter: "", value: 0 },
     { id: "deck1_piece6", letter: "", value: 0 },
   ];
+
   //function to display which players turn is
   function changeDisplayTurn() {
     if (firstPlayerTurn.is) {
@@ -315,6 +374,7 @@ $(document).ready(function () {
   //the tile will only be draggable when it's player's turn
   //check here for references https://jqueryui.com/draggable/
   $("#playButton").on("click", function () {
+    
     tilesPlayer1.forEach((tile) =>
       $(`#${tile.id}`).draggable({ disabled: true })
     );
@@ -333,37 +393,69 @@ $(document).ready(function () {
   });
 
   $("#endTurn").on("click", function () {
-    
+    let draft;
+    let submittedDirection ;
+    let gaps;
+    //player 1 validations
     if (firstPlayerTurn.is) {
       if (submitedLetters[0].player1.length > 0) {
-        if (isInRowDirection(submitedLetters[2].boardId)) {
+        submittedDirection = findDirection(submitedLetters[2].boardId);
+        if (submittedDirection === "row") {
           //sort letters by the column number
           rearrangeRowbyColumn(firstPlayerTurn.is)
           console.log (submitedLetters);
-          
+          //validation of player's move
+          if ( validateTheMove(submitedLetters[2].boardId,firstMove, submittedDirection) ) {
+            //check if is there are gaps among letters
+            gaps = getGaps(submitedLetters[2].boardId);
+            if (gaps.length === 0) {
+              draft = submitedLetters[0].player1.join('');
+              console.log(draft);
+              console.log("living la vida loca!")
+              //write an asyncronous function to handle the requisition 
+             /*  wordCheck(draft); */
+            }
+          }
         } else {
           //sort letters by the row number
           rearrangeColumnbyRow(firstPlayerTurn.is);
           console.log(submitedLetters);
+          if( validateTheMove(submitedLetters[2].boardId,firstMove, submittedDirection) ) {
+            gaps = getGaps(submitedLetters[2].boardId);
+            if (gaps.length === 0) {
+            draft = submitedLetters[0].player1.join('');
+            console.log(draft);
+            console.log("living wild")
+            }
+          } 
         }
       } else {
         console.log("player didn't play any tile");
       }
+      //player 2 validations
     } else {
       if (submitedLetters[1].player2.length > 0) {
-        if (isInRowDirection(submitedLetters[2].boardId)) {
+        if (submittedDirection === "row") {
           //sort letters by the column number
           rearrangeRowbyColumn(firstPlayerTurn.is);
           console.log(submitedLetters);
+          console.log( validateTheMove(submitedLetters[2].boardId, firstMove, submittedDirection) )
+
         } else {
           //sort letters by the row number
           rearrangeColumnbyRow(firstPlayerTurn.is);
           console.log(submitedLetters);
+          console.log( validateTheMove(submitedLetters[2].boardId, firstMove, submittedDirection) )
+
         }
       } else {
         console.log("player didn't play any tile");
       }
     }
+
+  /*   if(!firstMove) {
+      firstMove = true;
+    }; */
 
     changePlayer();
     changeDisplayTurn();
@@ -375,21 +467,26 @@ $(document).ready(function () {
 
   //function to get in which direction the player placed the letters
   //reference https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
-  function isInRowDirection(positions) {
+  function findDirection(positions) {
     let x = [];
     let y = [];
-    positions.forEach((position) => {
-      x.push(position.x);
-      y.push(position.y);
+    let direction = "not allowed";
+    positions.forEach(item => {
+      x.push(item.positionx);
+      y.push(item.positiony);
     });
 
     if (x.every((elem, i, arr) => elem === arr[0])) {
-      return true;
-    } else if (y.every((elem, i, arr) => elem === arr[0])) {
-      return false;
+      direction = "row"
+      
+    } else if (y.every((elem, i, arr) => elem == arr[0])) {
+      direction = "column"
+      
     }
+    return direction;
   }
 
+  //function for getting the values of the rows
   function selectTheRow(ids) {
     let query = [];
     ids.forEach((id) => query.push(id.positiony));
@@ -397,21 +494,93 @@ $(document).ready(function () {
     return query;
   }
 
+  //function for getting the values of the columns
   function selectTheColumn(ids) {
     let query = [];
     ids.forEach((id) => query.push(id.positionx));
     return query;
   }
 
-  //function to get drop events on the board
-  function find_letter(given_id) {
-    // Go through the 7 pieces,
-    for (var i = 0; i < 7; i++) {
-      // If we found the piece we're looking for, awesome!
-      if (game_tiles[i].id == given_id) {
-        // Just return its letter!
-        return game_tiles[i].letter;
+  //function to record letters place on the boardgame
+  function wordDraftCreator(id, letter, positionx, positiony, player) {
+    if (player) {
+      submitedLetters[0].player1.push(letter);
+      submitedLetters[0].playerId.push(id);
+      submitedLetters[2].boardId.push({ positionx, positiony });
+    } else {
+      submitedLetters[1].player2.push(letter);
+      submitedLetters[1].playerId.push(id);
+      submitedLetters[2].boardId.push({ positionx, positiony });
+    }
+  }
+
+  //function to delete letters from draft when removed from the boardgame
+  function takeWordFromDraft(id, player) {
+    if (player) {
+      let index = submitedLetters[0].playerId.indexOf(id);
+      if (index > -1) {
+        submitedLetters[0].player1.splice(index, 1);
+        submitedLetters[0].playerId.splice(index, 1);
+        submitedLetters[2].boardId.splice(index, 1);
       }
+    } else {
+      let index = submitedLetters[1].playerId.indexOf(id);
+      if (index > -1) {
+        submitedLetters[1].player2.splice(index, 1);
+        submitedLetters[1].playerId.splice(index, 1);
+        submitedLetters[2].boardId.splice(index, 1);
+      }
+    }
+  }
+
+  //function to rearrange array by column
+  function rearrangeRowbyColumn(player) {
+    if (player) {
+      let y = selectTheRow(submitedLetters[2].boardId);
+      let copyY = [...y];
+      let orderedY = copyY.sort((a, b) => a - b);
+      let rearrangedIndexes = [];
+      orderedY.forEach(item=> rearrangedIndexes.push(y.indexOf(item)) );
+      submitedLetters[2].boardId = rearrangedIndexes.map( i => submitedLetters[2].boardId[i]);
+      submitedLetters[0].player1 = rearrangedIndexes.map( i => submitedLetters[0].player1[i]);
+      submitedLetters[0].playerId = rearrangedIndexes.map( i => submitedLetters[0].playerId[i]);
+      return true
+    } else {
+      let y = selectTheRow(submitedLetters[2].boardId);
+      let copyY = [...y];
+      let orderedY = copyY.sort((a, b) => a - b);
+      let rearrangedIndexes = [];
+      orderedY.forEach(item=> rearrangedIndexes.push(y.indexOf(item)) )
+      submitedLetters[2].boardId = rearrangedIndexes.map( i => submitedLetters[2].boardId[i]);
+      submitedLetters[1].player2 = rearrangedIndexes.map( i => submitedLetters[1].player2[i]);
+      submitedLetters[1].playerId = rearrangedIndexes.map( i => submitedLetters[1].playerId[i]);
+      return true
+    }
+    
+  }
+
+  //function to rearrange array by row
+  function rearrangeColumnbyRow(player) {
+    if (player) {
+      let x = selectTheColumn(submitedLetters[2].boardId);
+      let copyX = [...x];
+      let orderedX = copyX.sort( (a,b) => a - b );
+      let rearrangedIndexes = [];
+      orderedX.forEach( item => rearrangedIndexes.push(x.indexOf(item)) );
+      submitedLetters[2].boardId = rearrangedIndexes.map( i => submitedLetters[2].boardId[i] );
+      submitedLetters[0].player1 = rearrangedIndexes.map( i => submitedLetters[0].player1[i]);
+      submitedLetters[0].playerId = rearrangedIndexes.map( i => submitedLetters[0].playerId[i]);
+      return true
+    } else {
+      let x = selectTheColumn(submitedLetters[2].boardId);
+      let copyX = [...x];
+      let orderedX = copyX.sort( (a,b) => a - b );
+      let rearrangedIndexes = [];
+      orderedX.forEach( item => rearrangedIndexes.push(x.indexOf(item)) );
+      submitedLetters[2].boardId = rearrangedIndexes.map( i => submitedLetters[2].boardId[i] );
+      submitedLetters[1].player2 = rearrangedIndexes.map( i => submitedLetters[1].player2[i]);
+      submitedLetters[1].playerId = rearrangedIndexes.map( i => submitedLetters[1].playerId[i]);
+      return true
     }
   }
 
@@ -423,6 +592,8 @@ $(document).ready(function () {
         console.log(data);
         if (classesNaoPermitidas.includes(data[0].class)) {
           allowedWord = false;
+        } else if (data.error) {
+          console.log("shakira, shakira")
         }
 
         //colocar o significado (só teste)
@@ -439,4 +610,4 @@ $(document).ready(function () {
       }
     );
   }
-});
+})
