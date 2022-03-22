4c2d62f87264d81f04533d391ed0d48e914b2675
washingtonSG <washington.gacho@dcomp.ufs.br>
2022-03-22 19:30:55 -0300

    new Client

--- backend/data/scoreData.js
+++ backend/data/scoreData.js
@@ -1,9 +1,26 @@
-const database = require('../infra/database');
+const database = require('../infra/database.js');
 
 exports.getScores = function () {
     database.connect()
-    database.query("SELECT * FROM alphazendinha.scores")
+    database.query(
+        "SELECT TOP 10 * FROM alphazendinha.scores \
+        ORDER BY score DESC")
     .then(result => result)
     .catch(e => console.error(e.stack))
     .then(() => database.end())
 }
+
+
+/*
+STENIO - 1
+JAIME - 2
+WELL  - 3
+WASH  - 4
+4
+5
+6
+7
+8
+10
+
+*/
--- backend/infra/database.js
+++ backend/infra/database.js
@@ -1,7 +1,7 @@
-const  pg = require('pg');
+const  {Client}= require('pg');
 require("dotenv").config()
 
-const client = pg.Client({
+const client = new Client({
     host:process.env.HOST,
     database:process.env.DATABASE,
     password: process.env.PASSWORD,
