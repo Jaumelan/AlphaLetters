e34df3a8acaf30f03430a0d11078ce299bb95778
washingtonSG <washington.gacho@dcomp.ufs.br>
2022-03-24 18:30:39 -0300

    savePlayer func on playerData file

--- backend/data/playerData.js
+++ backend/data/playerData.js
@@ -1,15 +1,16 @@
 const client = require('../infra/database');
 
-exports.getPlayers  = function () {
-    client.query(
-        "SELECT TOP 10 * FROM game.players \
-        ORDER BY score DESC")
-    .then(result => result)
-    .catch(e => console.error(e.stack))
-    .then(() => client.end())
-}
+// exports.getPlayers  = function () {
+//     client.query(
+//         "SELECT TOP 10 * FROM game.players \
+//         ORDER BY score DESC")
+//     .then(result => result)
+//     .catch(e => console.error(e.stack))
+//     .then(() => client.end())
+// }
 exports.savePlayer = async function (player) {
     try {
+        client = await client.connect()
         await client.query("INSERT INTO game.players (name, score, avatar_id) VALUES ($1,$2, $3);", player);
         res.status(201).send('OK ' );
         console.log('entrou');
