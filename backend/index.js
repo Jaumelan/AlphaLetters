0e2f6b93231fdac15a6611db196a8645c7cb676e
washingtonSG <washington.gacho@dcomp.ufs.br>
2022-03-24 07:50:36 -0300

    dotenv

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const port = 3000;
 const app = express();
+require("dotenv").config()
+console.log(process.env)
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
