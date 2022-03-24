d57056058692896c1bbd93970b78d5b64d4afab6
washingtonSG <washington.gacho@dcomp.ufs.br>
2022-03-24 20:44:31 -0300

    env

--- backend/infra/database.js
+++ backend/infra/database.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 console.log(process.env.USERNAME)
 const db =  pgp({
     host:process.env.HOST || 'localhost',
-    user: process.env.USER ||'postgres',
+    user: process.env.USERNAME ||'postgres',
     database:  process.env.DATABASE || 'alphazendinha',
     password:process.env.PASSWORD || 'Io.ri5'  ,
     port: process.env.PORT || 5432,
