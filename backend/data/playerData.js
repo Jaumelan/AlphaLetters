fd746b849caf44d72515f0a812031fb41c642a12
washingtonSG <washington.gacho@dcomp.ufs.br>
2022-03-24 17:38:36 -0300

    savePlayer func on playerData file

--- backend/data/playerData.js
+++ backend/data/playerData.js
@@ -10,12 +10,22 @@ exports.getPlayers  = function () {
     .then(() => database.end())
 }
 exports.savePlayer = function (player) {
-    let [name, score, avatar_id] = [...player]
-    database
-    .connect()
-    .then(() =>console.log('connected'))
-    .catch(err => console.error('connection error', err.stack))
-    .then(() => database.end())
+    try {
+        const client = await database.connect();
+        await client.query("INSERT INTO game.players (name, score, avatar_id) VALUES ($1,$2, $3);", player);
+        await client.query("INSERT INTO game.players (name, score, avatar_id) VALUES ($1,$2, $3);", player);
+        res.status(201).send('OK ' );
+        client.release();
+        } catch (err) {
+            console.error(err);
+            res.send("Error " + err);
+        }
+    }
+    // database
+    // .connect()
+    // .then(() =>console.log('connected'))
+    // .catch(err => console.error('connection error', err.stack))
+    // .then(() => database.end())
     // database.query(
     //     "INSERT INTO game.players(name, score, avatar_id) \
     //     VALUES($1,$2,$3)", [name, score, avatar_id])
@@ -24,10 +34,8 @@ exports.savePlayer = function (player) {
     //     .then(() => database.end())
     //     )
     // }
-}
-
 /*
-STENIO - 1
+DANUBIO - 1
 JAIME - 2
 WELL  - 3
 WASH  - 4
