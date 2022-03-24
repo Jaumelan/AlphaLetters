a0bd79b1659c8bc259b2e1e6f419decf15372565
washingtonSG <washington.gacho@dcomp.ufs.br>
2022-03-24 20:27:03 -0300

    savePlayer func on playerData file

--- backend/infra/database.js
+++ backend/infra/database.js
@@ -1,11 +1,11 @@
 const pgp = require('pg-promise')();
 require('dotenv').config()
 const db =  pgp({
-    host:process.env.HOST || 'localhost'  ,
-    database:  process.env.DATABASE || 'alphazendinha',
-    password:process.env.PASSWORD || 'Io.ri5'  ,
-    port: process.env.PORT || 5432,
-    user: process.env.USER ||'postgres',
+    host:'localhost',
+    database:'alphazendinha',
+    password:'Io.ri5',
+    port: 5432,
+    user:'postgres',
 });
 
 module.exports = db;
